69e90b33a92365256b25f348d82ade1c1e8df5f1
Hemnath Balasubramanian <hemnath@thepenindia.com>
2025-01-12 15:08:11 +0530

    initial commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Welcome from "~/components/welcome/Welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="h-screen overflow-auto bg-slate-900">
+      <Welcome />
+    </div>
+  );
 }
