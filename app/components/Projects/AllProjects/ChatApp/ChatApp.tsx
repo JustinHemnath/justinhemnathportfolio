c83ccb153c441a0aa58289cca88681894d3f56a9
Hemnath Balasubramanian <hemnath@thepenindia.com>
2025-02-01 22:52:47 +0530

    moved new user modal into new user component

--- app/components/Projects/AllProjects/ChatApp/ChatApp.tsx
+++ app/components/Projects/AllProjects/ChatApp/ChatApp.tsx
@@ -14,21 +14,10 @@ import SendMessage from "./SendMessage";
 import { CHAT_APP_EVENTS } from "~/constants/main.constants";
 import { io } from "socket.io-client";
 import moment from "moment";
-import { v4 as uuidv4 } from "uuid";
 import {
   chatBottomScroller,
   receivedMessageHandler,
 } from "~/utils/chatApp.utils";
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-  useDisclosure,
-  Input,
-} from "@heroui/react";
 
 const ChatApp = ({
   setIsLoggedIn,
@@ -45,52 +34,6 @@ const ChatApp = ({
 }: any) => {
   const socket = useSocketStore((state: any) => state.socket);
   const setSocket = useSocketStore((state) => state.setSocket);
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [newUserMessage, setNewUserMessage] = useState("");
-  const [newUserSelected, setNewUserSelected] = useState<any>(null);
-
-  function handleNewUserMessage(
-    e: any,
-    onClose: any,
-    newUserMessage: string,
-    newUserSelected: any,
-  ) {
-    e.preventDefault();
-
-    const newConversations = [...conversations];
-    const newMessage: TMessage = {
-      sender: userDetails.email,
-      receiver: newUserSelected.email,
-      sender_name: userDetails.userName,
-      receiver_name: newUserSelected.email,
-      message: newUserMessage,
-      id: uuidv4(),
-      sent_at: moment().format(),
-    };
-    // emit message to server
-    socket.emit(CHAT_APP_EVENTS.TO_SERVER, newMessage);
-
-    const newConversationItem = {
-      otherPersonEmail: newUserSelected.email,
-      otherPersonName: newUserSelected.name,
-      messages: [newMessage],
-      lastMessage: newMessage,
-    };
-    newConversations.push(newConversationItem);
-    newConversations.sort((a, b) => {
-      const aDate: any = new Date(a.lastMessage.sent_at);
-      const bDate: any = new Date(b.lastMessage.sent_at);
-      return bDate - aDate;
-    });
-
-    setConversations(newConversations);
-
-    // on close protocol
-    onClose();
-    setNewUserMessage("");
-    setNewUserSelected(null);
-    setCurrentConversation(newConversationItem);
-  }
 
   function handleLogout() {
     if (typeof window !== "undefined") {
@@ -174,9 +117,10 @@ const ChatApp = ({
                 {...{
                   allUsers,
                   conversations,
+                  setConversations,
                   setCurrentConversation,
-                  onOpen,
-                  setNewUserSelected,
+                  userDetails,
+                  socket,
                 }}
               />
             </div>
@@ -207,53 +151,6 @@ const ChatApp = ({
                 }}
               />
             </div>
-
-            <Modal isOpen={isOpen} size={"md"} onClose={onClose}>
-              <ModalContent>
-                {(onClose) => (
-                  <>
-                    <ModalHeader className="flex flex-col gap-1 text-center">
-                      Start a conversation with {newUserSelected.name}!
-                    </ModalHeader>
-                    <ModalBody className="text-xl">
-                      <p>
-                        This will start a new conversation with{" "}
-                        {newUserSelected.name}
-                      </p>
-                      <form
-                        onSubmit={(e: any) =>
-                          handleNewUserMessage(
-                            e,
-                            onClose,
-                            newUserMessage,
-                            newUserSelected,
-                          )
-                        }
-                        className="my-10 flex items-center gap-3"
-                      >
-                        <Input
-                          label="Say a hi!"
-                          type="text"
-                          onChange={(e: any) =>
-                            setNewUserMessage(e.target.value)
-                          }
-                          value={newUserMessage}
-                          isRequired
-                        />
-                        <Button className="" type="submit">
-                          Send
-                        </Button>
-                      </form>
-                      <p>
-                        {newUserSelected.name} will receive your message when
-                        they log in.
-                      </p>
-                    </ModalBody>
-                    <ModalFooter></ModalFooter>
-                  </>
-                )}
-              </ModalContent>
-            </Modal>
           </div>
         </div>
       ) : (
--- app/components/Projects/AllProjects/ChatApp/NewUserSelect.tsx
+++ app/components/Projects/AllProjects/ChatApp/NewUserSelect.tsx
@@ -1,5 +1,5 @@
 import { Select, SelectItem } from "@heroui/react";
-import type { TConversation } from "~/stores/chatapp.store";
+import type { TConversation, TMessage } from "~/stores/chatapp.store";
 import {
   Dropdown,
   DropdownTrigger,
@@ -8,14 +8,32 @@ import {
   Button,
 } from "@heroui/react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Input,
+} from "@heroui/react";
+import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
+import { CHAT_APP_EVENTS } from "~/constants/main.constants";
+import { useState } from "react";
 
 const NewUserSelect = ({
   allUsers,
   conversations,
+  setConversations,
   setCurrentConversation,
-  onOpen,
-  setNewUserSelected,
+  userDetails,
+  socket,
 }: any) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [newUserSelected, setNewUserSelected] = useState<any>(null);
+  const [newUserMessage, setNewUserMessage] = useState("");
+
   function handleUserSelected(selectedUserEmail: string) {
     const matchingConvo = conversations.find(
       (convo: TConversation) => convo.otherPersonEmail === selectedUserEmail,
@@ -33,6 +51,49 @@ const NewUserSelect = ({
       });
       onOpen();
     }
+  }
+
+  function handleNewUserMessage(
+    e: any,
+    onClose: any,
+    newUserMessage: string,
+    newUserSelected: any,
+  ) {
+    e.preventDefault();
+
+    const newConversations = [...conversations];
+    const newMessage: TMessage = {
+      sender: userDetails.email,
+      receiver: newUserSelected.email,
+      sender_name: userDetails.userName,
+      receiver_name: newUserSelected.email,
+      message: newUserMessage,
+      id: uuidv4(),
+      sent_at: moment().format(),
+    };
+    // emit message to server
+    socket.emit(CHAT_APP_EVENTS.TO_SERVER, newMessage);
+
+    const newConversationItem = {
+      otherPersonEmail: newUserSelected.email,
+      otherPersonName: newUserSelected.name,
+      messages: [newMessage],
+      lastMessage: newMessage,
+    };
+    newConversations.push(newConversationItem);
+    newConversations.sort((a, b) => {
+      const aDate: any = new Date(a.lastMessage.sent_at);
+      const bDate: any = new Date(b.lastMessage.sent_at);
+      return bDate - aDate;
+    });
+
+    setConversations(newConversations);
+
+    // on close protocol
+    onClose();
+    setNewUserMessage("");
+    setNewUserSelected(null);
+    setCurrentConversation(newConversationItem);
   }
 
   return (
@@ -60,6 +121,50 @@ const NewUserSelect = ({
           ))}
         </DropdownMenu>
       </Dropdown>
+
+      <Modal isOpen={isOpen} size={"md"} onClose={onClose}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 text-center">
+                Start a conversation with {newUserSelected.name}!
+              </ModalHeader>
+              <ModalBody className="text-xl">
+                <p>
+                  This will start a new conversation with {newUserSelected.name}
+                </p>
+                <form
+                  onSubmit={(e: any) =>
+                    handleNewUserMessage(
+                      e,
+                      onClose,
+                      newUserMessage,
+                      newUserSelected,
+                    )
+                  }
+                  className="my-10 flex items-center gap-3"
+                >
+                  <Input
+                    label="Say a hi!"
+                    type="text"
+                    onChange={(e: any) => setNewUserMessage(e.target.value)}
+                    value={newUserMessage}
+                    isRequired
+                  />
+                  <Button className="" type="submit">
+                    Send
+                  </Button>
+                </form>
+                <p>
+                  {newUserSelected.name} will receive your message when they log
+                  in.
+                </p>
+              </ModalBody>
+              <ModalFooter></ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
