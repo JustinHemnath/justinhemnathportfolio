20d8dce809b6e3a429ff2bf128250612887760f2
Hemnath Balasubramanian <hemnath@thepenindia.com>
2025-02-02 01:13:24 +0530

    added firefox warning alert

--- app/components/welcome/Welcome.tsx
+++ app/components/welcome/Welcome.tsx
@@ -8,14 +8,38 @@ import Sidebar from "../Sidebar/Sidebar";
 import { SECTIONS } from "~/constants/main.constants";
 import PageWrapper from "../PageWrapper";
 import Contact from "../Contact/Contact";
+import { Alert } from "@heroui/react";
+import { IoMdClose } from "react-icons/io";
 
 const Welcome = () => {
   const sidebarWidth = 40;
   const [pageInView, setPageInView] = useState(SECTIONS.ABOUT);
+  const [initialAlertVisible, setInitialAlertVisible] = useState(true);
   const app = initializeApp(firebaseConfig);
 
   return (
     <div>
+      {initialAlertVisible ? (
+        <div className="fixed right-0 top-0 flex w-[50%] p-10">
+          <Alert
+            color={"default"}
+            title={
+              "Please view this site on any browser other than Firefox as firefox compatibility is not added"
+            }
+            classNames={{
+              base: "!bg-white",
+              title: "text-xl",
+            }}
+            endContent={
+              <IoMdClose
+                className="cursor-pointer text-2xl text-black"
+                onClick={() => setInitialAlertVisible(false)}
+              />
+            }
+          />
+        </div>
+      ) : null}
+
       <Sidebar {...{ sidebarWidth, pageInView, setPageInView }} />
 
       {/* main pages  */}
